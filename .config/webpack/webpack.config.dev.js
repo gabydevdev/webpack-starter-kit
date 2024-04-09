844c74c2edd0eb0c39cf59c1b889bbd5ac9645f3
Gaby F. <1626284+iamgaby7521@users.noreply.github.com>
2024-04-09 03:33:40 -0700

    delete 3 files, create 3 files and update 7 files

--- .config/webpack/webpack.config.dev.js
+++ .config/webpack/webpack.config.dev.js
@@ -1,58 +1,40 @@
-const Path = require('path');
-const Webpack = require('webpack');
-const { merge } = require('webpack-merge');
-const ESLintPlugin = require('eslint-webpack-plugin');
-const StylelintPlugin = require('stylelint-webpack-plugin');
+const Path = require("path");
+const Webpack = require("webpack");
+const { merge } = require("webpack-merge");
+const ESLintPlugin = require("eslint-webpack-plugin");
+const StylelintPlugin = require("stylelint-webpack-plugin");
 
-const common = require('./webpack.common.js');
+const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-  target: 'web',
-  mode: 'development',
-  devtool: 'eval-cheap-source-map',
-  output: {
-    chunkFilename: 'js/[name].chunk.js',
-  },
-  devServer: {
-    client: {
-      logging: 'error',
-    },
-    hot: true,
-  },
-  plugins: [
-    new Webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development'),
-    }),
-    new ESLintPlugin({
-      extensions: 'js',
-      emitWarning: true,
-      files: Path.resolve(__dirname, '../src'),
-    }),
-    new StylelintPlugin({
-      files: Path.join('src', '**/*.s?(a|c)ss'),
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: Path.resolve(__dirname, '../src'),
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.s?css$/i,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
-          'postcss-loader',
-          'sass-loader',
-        ],
-      },
-    ],
-  },
+	mode: "development",
+	devtool: "eval-cheap-source-map",
+	devServer: {
+		static: {
+			directory: Path.join(__dirname, "../../build"),
+		},
+		hot: true,
+	},
+	plugins: [
+		new Webpack.HotModuleReplacementPlugin(),
+		new ESLintPlugin({
+			extensions: ["js"],
+			exclude: "node_modules",
+			context: Path.resolve(__dirname, "../../src"), // Specify the directory to lint
+			eslintPath: require.resolve("eslint"),
+			overrideConfigFile: Path.resolve(__dirname, "../eslint.config.js"), // Path to your ESLint configuration
+		}),
+		new StylelintPlugin({
+			files: "src/**/*.scss",
+			configFile: Path.resolve(__dirname, "../stylelint.config.js"), // Path to your Stylelint configuration
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.s?css$/,
+				use: ["style-loader", "css-loader", "sass-loader"],
+			},
+		],
+	},
 });
--- .config/webpack/webpack.config.prod.js
+++ .config/webpack/webpack.config.prod.js
@@ -1,37 +1,30 @@
-const Webpack = require('webpack');
-const { merge } = require('webpack-merge');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const common = require('./webpack.common.js');
+const Path = require("path");
+const Webpack = require("webpack");
+const { merge } = require("webpack-merge");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-  mode: 'production',
-  devtool: 'source-map',
-  stats: 'errors-only',
-  bail: true,
-  output: {
-    filename: 'js/[name].[chunkhash:8].js',
-    chunkFilename: 'js/[name].[chunkhash:8].chunk.js',
-  },
-  plugins: [
-    new Webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production'),
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].[chunkhash:8].css',
-      chunkFilename: 'css/[name].[chunkhash:8].chunk.js',
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-      {
-        test: /\.s?css/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
-      },
-    ],
-  },
+	mode: "production",
+	devtool: "source-map",
+	output: {
+		path: Path.join(__dirname, "../../build"),
+		filename: "js/[name].[contenthash].js",
+		chunkFilename: "js/[name].[contenthash].chunk.js",
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: "css/[name].[contenthash].css",
+			chunkFilename: "css/[name].[contenthash].chunk.css",
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.s?css$/,
+				use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+			},
+		],
+	},
 });
