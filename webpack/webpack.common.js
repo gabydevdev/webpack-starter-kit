201a619b45bff159c626df9da72fc9c409d58ea9
Gaby F. <1626284+iamgaby7521@users.noreply.github.com>
2024-04-18 22:40:19 -0700

    * feat(about.html): add about us page content * feat(contact.html): add contact us page content * feat(home.html): add home page content * feat(team.html): add our team page content * refactor(main.js): rename index.js to main.js * refactor(main.scss): rename index.scss to main.scss * refactor(webpack.common.js): refactor entry points setup and html plugins configuration in webpack common config

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,12 +1,42 @@
 const Path = require("path");
+const fs = require("fs");
+const glob = require("glob");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
+function setupEntriesAndHtmlPlugins() {
+	const htmlFiles = glob.sync(Path.resolve(__dirname, "../src/*.html"));
+	const entries = {};
+	const htmlPlugins = [];
+
+	htmlFiles.forEach((file) => {
+		const baseName = Path.basename(file, ".html");
+		const jsFilePath = Path.resolve(__dirname, `../src/js/${baseName}.js`);
+
+		if (fs.existsSync(jsFilePath)) {
+			entries[baseName] = jsFilePath; // Add JS file to entry if it exists
+		}
+
+		htmlPlugins.push(
+			new HtmlWebpackPlugin({
+				template: file,
+				filename: `${baseName}.html`,
+				chunks: fs.existsSync(jsFilePath) ? [baseName] : [], // Include JS only if it exists
+			})
+		);
+	});
+
+	// Always include the main.js for shared functionality
+	entries["main"] = Path.resolve(__dirname, "../src/js/main.js");
+
+	return { entries, htmlPlugins };
+}
+
+const { entries, htmlPlugins } = setupEntriesAndHtmlPlugins();
+
 module.exports = {
-	entry: {
-		app: Path.resolve(__dirname, "../src/scripts/index.js"),
-	},
+	entry: entries,
 	output: {
 		path: Path.join(__dirname, "../build"),
 		filename: "js/[name].js",
@@ -14,17 +44,17 @@ module.exports = {
 	plugins: [
 		new CleanWebpackPlugin(),
 		new CopyWebpackPlugin({
-			patterns: [{ from: Path.resolve(__dirname, "../public"), to: "public" }],
+			patterns: [
+				{ from: Path.resolve(__dirname, "../public"), to: "public" },
+			],
 		}),
-		new HtmlWebpackPlugin({
-			template: Path.resolve(__dirname, "../src/index.html"),
-		}),
+		...htmlPlugins
 	],
 	resolve: {
 		alias: {
 			"~": Path.resolve(__dirname, "../src"),
 		},
-		modules: [Path.resolve(__dirname, '../node_modules'), 'node_modules'],
+		modules: [Path.resolve(__dirname, "../node_modules"), "node_modules"],
 	},
 	module: {
 		rules: [
